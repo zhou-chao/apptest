1c1e44df4451613a19304c4c0d03e922bdf329bd
Chao Zhou <brycezhou@tencent.com>
2019-08-30 10:51:55 +0800

    Add main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *  FILENAME:      main.cpp
+ * 
+ *  AUTHORS:       Zhou Chao    START DATE: Friday August 30th 2019
+ * 
+ *  LAST MODIFIED: Friday August 30th 2019 10:50:34 am
+ * 
+ *  CONTACT:       brycezhou@tencent.com
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello" << std::endl;    
+}
